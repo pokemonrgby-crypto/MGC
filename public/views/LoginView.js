bf6ecfd6376df825f2400f5da6bc52e511e4db20
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:55:39 +0900

    Update LoginView.js

--- public/views/LoginView.js
+++ public/views/LoginView.js
@@ -2,7 +2,16 @@ export const LoginView = () => `
     <div class="form-container">
         <h2>로그인</h2>
         <form id="login-form">
-            </form>
+            <div class="input-group">
+                <label for="nickname">닉네임</label>
+                <input type="text" id="nickname" name="nickname" required>
+            </div>
+            <div class="input-group">
+                <label for="password">비밀번호</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn">로그인</button>
+        </form>
         <div id="message" class="message-area" style="display:none;"></div>
         <p class="switch-form">계정이 없으신가요? <a href="/" data-link>회원가입</a></p>
     </div>
