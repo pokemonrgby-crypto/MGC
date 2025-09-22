cfbf2d404887d3ae4b7c29aa69d6c4366a559803
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:49:46 +0900

    Create LoginView.js

---
+++ public/views/LoginView.js
@@ -0,0 +1,9 @@
+export const LoginView = () => `
+    <div class="form-container">
+        <h2>로그인</h2>
+        <form id="login-form">
+            </form>
+        <div id="message" class="message-area" style="display:none;"></div>
+        <p class="switch-form">계정이 없으신가요? <a href="/" data-link>회원가입</a></p>
+    </div>
+`;
