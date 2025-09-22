4a2559feb172134dcc0410b89c3b95a2f0f07a4e
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:42:37 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,13 +1,12 @@
 import { navigateTo } from './router.js';
-import { handleRegisterSubmit } from './auth.js';
-// import { handleLoginSubmit } from './auth.js'; // 나중에 추가
+import { handleRegisterSubmit, handleLoginSubmit } from './auth.js'; // handleLoginSubmit 추가
 
-// 모든 이벤트는 document.body에 한 번만 연결해서 관리
 document.addEventListener('submit', (event) => {
     if (event.target.id === 'register-form') {
         handleRegisterSubmit(event);
     }
-    // if (event.target.id === 'login-form') {
-    //     handleLoginSubmit(event);
-    // }
+    // 로그인 폼 제출 이벤트 처리 추가
+    if (event.target.id === 'login-form') {
+        handleLoginSubmit(event);
+    }
 });
