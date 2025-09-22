9313198001ec3ff53b0e69b59f3ddc2cad9fcc4e
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 14:06:32 +0900

    Update MainView.js

--- public/views/MainView.js
+++ public/views/MainView.js
@@ -1,3 +1,11 @@
-// MainView.js
-export const MainView = () => `<h2>메인 화면</h2><p>세계관 목록이 표시됩니다.</p>`;
-
+export const MainView = () => `
+    <div class="view-container">
+        <h2>창조된 세계들</h2>
+        <div id="world-list-container">
+            <div class="spinner-container">
+                <div class="spinner"></div>
+                <p>세계들을 불러오는 중...</p>
+            </div>
+        </div>
+    </div>
+`;
