799841fe79d739d79e6bb88be74edc47090a5da2
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:51:16 +0900

    Update MainView.js

--- public/views/MainView.js
+++ public/views/MainView.js
@@ -1,8 +1,3 @@
 // MainView.js
 export const MainView = () => `<h2>메인 화면</h2><p>세계관 목록이 표시됩니다.</p>`;
 
-// CreateView.js
-export const CreateView = () => `<h2>생성 화면</h2><p>세계관, 캐릭터를 생성합니다.</p>`;
-
-// MyInfoView.js
-export const MyInfoView = () => `<h2>내 정보</h2><p>API 키 설정, 로그아웃 등을 합니다.</p>`;
