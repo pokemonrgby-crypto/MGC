e2e910d5a367a96f7bbc6bc840cf80be83d49297
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:49:36 +0900

    Create RegisterView.js

---
+++ public/views/RegisterView.js
@@ -0,0 +1,9 @@
+export const RegisterView = () => `
+    <div class="form-container">
+        <h2>회원가입</h2>
+        <form id="register-form">
+            </form>
+        <div id="message" class="message-area" style="display:none;"></div>
+        <p class="switch-form">이미 계정이 있으신가요? <a href="/login" data-link>로그인</a></p>
+    </div>
+`;
