11dfa2b81b63da00fd117c2c4dab586b9abfc7ff
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 10:49:57 +0900

    Create MainLayout.js

---
+++ public/views/MainLayout.js
@@ -0,0 +1,14 @@
+export const MainLayout = () => `
+    <main id="content-area" class="content-area"></main>
+    <nav class="bottom-nav">
+        <a href="/main" class="nav-item" data-link>
+            <svg>...</svg> <span class="label">메인</span>
+        </a>
+        <a href="/create" class="nav-item" data-link>
+            <svg>...</svg> <span class="label">생성</span>
+        </a>
+        <a href="/my-info" class="nav-item" data-link>
+            <svg>...</svg> <span class="label">내 정보</span>
+        </a>
+    </nav>
+`;
