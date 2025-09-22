9c6aa3f083fbd9411c9b7320a6857d6f2b3a9da5
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 11:00:07 +0900

    Update MyInfoView.js

--- public/views/MyInfoView.js
+++ public/views/MyInfoView.js
@@ -1,1 +1,7 @@
-export const MyInfoView = () => `<h2>내 정보</h2><p>API 키 설정, 로그아웃 등을 합니다.</p>`;
+export const MyInfoView = () => `
+    <div class="my-info-container">
+        <h2>내 정보</h2>
+        <p>API 키 설정, 계정 관리 등을 할 수 있습니다.</p>
+        <button id="logout-button" class="btn">로그아웃</button>
+    </div>
+`;
